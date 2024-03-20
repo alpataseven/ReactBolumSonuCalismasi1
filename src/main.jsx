a060998db32467e6edf0ba28e310c93965c65659
Alperen ATASEVEN <alperenataseven.3@gmail.com>
2024-03-21 02:54:24 +0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import "@picocss/pico"
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+
+//TODO: Pico css kullanilsin
+//TODO: Ana Görsel Olsun
+//TODO: Altında 5 adet küçük görsel olsun
+//TODO: kucuk gorsellerden birine tiklaninca ana gorsel degissin
+//TODO: gorseller lorem picsumdan alinsin
+//TODO: default olarak 200 no lu id secilsin
+//TODO: gorseller array olarak [200, 201, 202, 203, 204]
